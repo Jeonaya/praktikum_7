47c4a739f5271bdc29c400e0d5d15549257fc392
Jeonaya <2400016006@webmail.uad.ac.id>
2025-12-09 22:56:05 +0700

    ar

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 
-// https://vite.dev/config/
+// ahttps://vite.dev/config/
 export default defineConfig({
   base : '/Jeonaya/praktikum_7/',
   plugins: [react(), tailwindcss(),],
